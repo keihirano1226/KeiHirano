3bd7520d71ea5e44c6e4520215a81005e20d61a6
Kei Hirano <hirano.syuukatu1226@gmail.com>
2019-10-04 16:04:25 +0900

    透視投影行列関連書きかけ追加

---
+++ pythonscript/ImageTool/Project_depth.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+import cv2
+import glob
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+
+if __name__ == '__main__':
+    basepass = sys.argv[1]
+    depthimage_pass = sys.ragv[1] + depth_mirror
+    csvpass = sys.argv[1] + "/pos/pos.csv"
